80d175ae54ce48e747f28006f5ec6421c7cbc301
Jp <167816534+JuanCll09@users.noreply.github.com>
2025-11-18 01:24:03 -0600

    Change API base URL from HTTPS to HTTP

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // --- FASE FINAL: CORRECCIÓN DE MIXED CONTENT ---
     // Usamos el protocolo HTTPS para evitar el bloqueo del navegador.
     // Esto funciona porque la llamada inicial de verificación es bloqueada por el navegador.
-    const API_BASE_URL = 'https://18.221.215.255/proyecto_uaq'; 
+    const API_BASE_URL = 'http://18.221.215.255/proyecto_uaq';
 
     export default API_BASE_URL;
